edfc81dea7ddaa11fc367bccfa3b234604ce1ed3
Ian Baldwin <ian@iantbaldw.in>
2019-05-04 12:50:51 -0400

    Some changes, probably for Greg

--- echo/echo_server.py
+++ echo/echo_server.py
@@ -1,18 +1,18 @@
 """Simple Echo Service for testing GRPC servers."""
 
-from pairity.echo.v1 import echo_api_pb2_grpc as echo_grpc, echo_api_pb2 as echo_api #type: ignore
-from pairity.utils.grpc_server import serve, config
-from pairity.utils.jwt import JwtVerifier
-
 import grpc
+from pairity.echo.v1 import echo_api_pb2 as echo_api
+from pairity.echo.v1 import echo_api_pb2_grpc as echo_grpc  # type: ignore
+from pairity.utils.grpc_server import config, serve
+from pairity.utils.jwt import JwtVerifier
 
 if __name__ == '__main__':
     serve()
 
+print(config.cognito)
 class EchoAPIServicer(echo_grpc.EchoAPIServicer):
     """Just echo requests."""
-    VERIFIER = JwtVerifier(config.cognito_config_user_pool_id, config.aws_config_region)
-
+    VERIFIER = JwtVerifier(config.cognito.user_pool_id, config.aws.region)
 
     @VERIFIER.require_jwt
     def Echo(self, request: echo_api.EchoRequest, context: grpc.RpcContext) -> echo_api.EchoResponse:
@@ -34,4 +34,5 @@ class EchoAPIServicer(echo_grpc.EchoAPIServicer):
         to_echo = request.something
         resp = echo_api.EchoResponse(something=to_echo)
         return resp
+
 
